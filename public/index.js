76c03477af17c3ef466dd6645e5477f587088309
Graham Schilling <gschilli@andrew.cmu.edu>
2014-09-11 08:14:06 -0400

    json example

--- public/index.js
+++ public/index.js
@@ -11,3 +11,36 @@ $(".el").on("mouseenter", function() {
     $(this).css("background-color","red");
 });
 
+
+function doXMLHttpRequest() {
+  var xhr = new XMLHttpRequest(); 
+
+  xhr.onreadystatechange=function()  {
+   if (xhr.readyState==4) {
+     if(xhr.status == 200) {
+        processResponse(xhr.responseText);
+    } else {
+      responseArea.innerHTML="Error code " + xhr.status;
+    }
+   }
+  }
+  xhr.open("GET", "data.json", true); 
+  xhr.send(null); 
+} 
+
+function processResponse(responseJSON) {
+        var responseObject = JSON.parse(responseJSON);
+        var displayText = 
+                "Today's Prices: " 
+                + responseObject.stocks.length 
+                + " stocks:<ol>";
+        for (var i = 0; i<responseObject.stocks.length; i++) {
+                var stock = responseObject.stocks[i];
+                displayText += "<li>"
+                                        +stock.stockName + " " 
+                                        + stock.price + "<\/li>";
+                }
+        displayText += "<\/ol>";
+        document.getElementById("responseArea").innerHTML = displayText;
+}
+
