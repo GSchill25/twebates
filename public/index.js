cc313dd9b7022611bb716c3dcec5506bad02ee36
Graham Schilling <gschilli@andrew.cmu.edu>
2014-09-15 17:54:43 -0400

    new api

--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
+window.setInterval(refresh, 2000);
 
 //Load Json with jquery
-$var timerID = setInterval(function() {    // do once original document loaded and ready
+$(function refresh() {    // do once original document loaded and ready
                 $.getJSON("http://btc.blockr.io/api/v1/coin/info", function(responseObject, diditwork) {
                         console.log(diditwork);
-                        var displayText = responseObject.data.coin.name          
+                        var displayText = responseObject.data.coin.name             
                 $("#responseArea2").html(displayText);
                 } );  // getJSON
-  }, 2000 ); // onReady
+        } );  // click
+  } ); // onReady
 
