4835c524e24acd822cc0d3980d03b357b9c120c4
Graham Schilling <gschilli@andrew.cmu.edu>
2014-12-10 17:24:57 -0500

    db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 module.exports = {
-	'url' : //'mongodb://localhost:27017/nodesample'
-	'mongodb://admin:p3s2VUa5123Q@http://nodesample-gschilli.rhcloud.com:8000/nodesample'
+	'url' : 'mongodb://admin:p3s2VUa5123Q@127.9.90.2:27017/nodesample'
+	//'mongodb://localhost:27017/nodesample'
+	
+
   
 }
 //pay need to change host
