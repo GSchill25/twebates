c6638e83fc3719b57907f6d1788535c0a7eb553c
Graham Schilling <gschilli@andrew.cmu.edu>
2014-12-10 22:43:20 -0500

    final touches

--- config/database.js
+++ config/database.js
@@ -1,3 +1,6 @@
+	//'url' : 'mongodb://admin:p3s2VUa5123Q@127.9.90.2:27017/nodesample'
+	//'mongodb://localhost:27017/nodesample'
+
 
 var util = require("util");
 var mongoClient = require('mongodb').MongoClient;
